977b5aea6b66157348047df92d8f924a9a1ef8d7
dmitrimeili <dmitri.meili@cpnv.ch>
2019-06-17 16:55:52 +0200

    wip

---
+++ Ballon - Base/Ballon/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ballon
+{
+    public partial class frmBallon : Form
+    
+    {
+        int tick=0;
+        public frmBallon()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frmBallon_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrTicTac_Tick(object sender, EventArgs e)
+        {
+            tick++;
+            label1.Text = tick.ToString();
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            /*comment faire stop et start*/
+             
+            tmrTicTac.Enabled = true;
+            label1.Text = tick.ToString();
+            cmdStart.Text= "stop";
+         
+
+    
+        }
+    }
+}
