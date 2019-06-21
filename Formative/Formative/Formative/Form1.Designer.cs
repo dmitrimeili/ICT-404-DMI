7a3aa1b75e3c2c758e843dfa2456edb87e2b3eab
dmitrimeili <dmitri.meili@cpnv.ch>
2019-06-21 16:54:52 +0200

    wip

---
+++ Formative/Formative/Formative/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Formative
+{
+    partial class frmCourses
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuantite = new System.Windows.Forms.TextBox();
+            this.cboArticle = new System.Windows.Forms.ComboBox();
+            this.cmdAjouter = new System.Windows.Forms.Button();
+            this.lstCourses = new System.Windows.Forms.ListBox();
+            this.cmdImprimer = new System.Windows.Forms.Button();
+            this.cmdSupprimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtQuantite
+            // 
+            this.txtQuantite.Location = new System.Drawing.Point(25, 25);
+            this.txtQuantite.Name = "txtQuantite";
+            this.txtQuantite.Size = new System.Drawing.Size(22, 20);
+            this.txtQuantite.TabIndex = 0;
+            // 
+            // cboArticle
+            // 
+            this.cboArticle.FormattingEnabled = true;
+            this.cboArticle.Location = new System.Drawing.Point(53, 24);
+            this.cboArticle.Name = "cboArticle";
+            this.cboArticle.Size = new System.Drawing.Size(121, 21);
+            this.cboArticle.TabIndex = 1;
+            // 
+            // cmdAjouter
+            // 
+            this.cmdAjouter.Location = new System.Drawing.Point(25, 68);
+            this.cmdAjouter.Name = "cmdAjouter";
+            this.cmdAjouter.Size = new System.Drawing.Size(149, 23);
+            this.cmdAjouter.TabIndex = 2;
+            this.cmdAjouter.Text = "Ajouter";
+            this.cmdAjouter.UseVisualStyleBackColor = true;
+            this.cmdAjouter.Click += new System.EventHandler(this.cmdAjouter_Click);
+            // 
+            // lstCourses
+            // 
+            this.lstCourses.FormattingEnabled = true;
+            this.lstCourses.Location = new System.Drawing.Point(25, 111);
+            this.lstCourses.Name = "lstCourses";
+            this.lstCourses.Size = new System.Drawing.Size(149, 134);
+            this.lstCourses.TabIndex = 3;
+            // 
+            // cmdImprimer
+            // 
+            this.cmdImprimer.Location = new System.Drawing.Point(25, 296);
+            this.cmdImprimer.Name = "cmdImprimer";
+            this.cmdImprimer.Size = new System.Drawing.Size(149, 23);
+            this.cmdImprimer.TabIndex = 4;
+            this.cmdImprimer.Text = "Imprimer";
+            this.cmdImprimer.UseVisualStyleBackColor = true;
+            this.cmdImprimer.Click += new System.EventHandler(this.cmdImprimer_Click);
+            // 
+            // cmdSupprimer
+            // 
+            this.cmdSupprimer.Location = new System.Drawing.Point(25, 267);
+            this.cmdSupprimer.Name = "cmdSupprimer";
+            this.cmdSupprimer.Size = new System.Drawing.Size(149, 23);
+            this.cmdSupprimer.TabIndex = 5;
+            this.cmdSupprimer.Text = "Supprimer";
+            this.cmdSupprimer.UseVisualStyleBackColor = true;
+            this.cmdSupprimer.Click += new System.EventHandler(this.cmdSupprimer_Click);
+            // 
+            // frmCourses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 376);
+            this.Controls.Add(this.cmdSupprimer);
+            this.Controls.Add(this.cmdImprimer);
+            this.Controls.Add(this.lstCourses);
+            this.Controls.Add(this.cmdAjouter);
+            this.Controls.Add(this.cboArticle);
+            this.Controls.Add(this.txtQuantite);
+            this.Name = "frmCourses";
+            this.Text = "Courses";
+            this.Load += new System.EventHandler(this.frmCourses_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuantite;
+        private System.Windows.Forms.ComboBox cboArticle;
+        private System.Windows.Forms.Button cmdAjouter;
+        private System.Windows.Forms.ListBox lstCourses;
+        private System.Windows.Forms.Button cmdImprimer;
+        private System.Windows.Forms.Button cmdSupprimer;
+    }
+}
+
---
+++ Formative/Formative/Formative/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formative
+{
+    public partial class frmCourses : Form
+    {
+        public frmCourses()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void frmCourses_Load(object sender, EventArgs e)
+        {
+            
+            cboArticle.Items.Add("Pain");
+            cboArticle.Items.Add("Beurre");
+            cboArticle.Items.Add("Lait");
+            cboArticle.Items.Add("Confiture");
+            cboArticle.Items.Add("Fromage");
+            cboArticle.Items.Add("Oeufs");
+            cboArticle.Items.Add("Farine");
+            cboArticle.Items.Add("Sucre");
+            cboArticle.Items.Add("Viande");
+   
+        }
+
+        private void cmdAjouter_Click(object sender, EventArgs e)
+        {
+            int quantite;
+            string ajout;
+            if (cboArticle.SelectedIndex == -1)
+            {
+                MessageBox.Show("Sélectionnez un article");
+                return;
+            }
+            else if (txtQuantite.Text==""||!int.TryParse(txtQuantite.Text, out quantite))
+            {
+                MessageBox.Show("Introduisez une quantité");
+                return;
+            }
+            else
+            {
+               
+                lstCourses.Items.Add(cboArticle.SelectedItem+": "+quantite.ToString());
+                ajout = cboArticle.SelectedItem.ToString();
+                cboArticle.Items.Remove(ajout);
+                txtQuantite.Text = "";
+                cboArticle.SelectedIndex = -1;
+            }
+        }
+
+        private void cmdSupprimer_Click(object sender, EventArgs e)
+        {
+            string article;
+            int longueur;
+
+            if (lstCourses.SelectedIndex != -1)
+            {
+                article = lstCourses.SelectedItem.ToString();
+                lstCourses.Items.Remove(article);
+                longueur = article.IndexOf(":");
+                article =article.Substring(0, longueur);
+                cboArticle.Items.Add(article);
+               
+            }
+            else
+            {
+                MessageBox.Show("Veulliez selectionner un article");
+            }
+        }
+
+        private void cmdImprimer_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cette fonction n'est pas encore disponible");
+        }
+    }
+}
