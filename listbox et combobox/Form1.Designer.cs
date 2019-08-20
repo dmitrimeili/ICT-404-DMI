93dda6354634938dc9f9d1772c08a12337edcc20
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-08-20 14:31:23 +0200

    WIP

---
+++ listbox et combobox/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace listbox_et_combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbo1 = new System.Windows.Forms.ComboBox();
+            this.cbo2 = new System.Windows.Forms.ComboBox();
+            this.cmdAjouter = new System.Windows.Forms.Button();
+            this.cmdEffacer = new System.Windows.Forms.Button();
+            this.cmdSupprimer = new System.Windows.Forms.Button();
+            this.cmdQuitter = new System.Windows.Forms.Button();
+            this.lst1 = new System.Windows.Forms.ListBox();
+            this.lst2 = new System.Windows.Forms.ListBox();
+            this.lblNbEcoles = new System.Windows.Forms.Label();
+            this.txtNbEcole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbo1
+            // 
+            this.cbo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cbo1.FormattingEnabled = true;
+            this.cbo1.Location = new System.Drawing.Point(41, 55);
+            this.cbo1.Name = "cbo1";
+            this.cbo1.Size = new System.Drawing.Size(178, 24);
+            this.cbo1.TabIndex = 0;
+            // 
+            // cbo2
+            // 
+            this.cbo2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.cbo2.FormattingEnabled = true;
+            this.cbo2.Location = new System.Drawing.Point(41, 368);
+            this.cbo2.Name = "cbo2";
+            this.cbo2.Size = new System.Drawing.Size(178, 24);
+            this.cbo2.TabIndex = 1;
+            // 
+            // cmdAjouter
+            // 
+            this.cmdAjouter.Location = new System.Drawing.Point(337, 56);
+            this.cmdAjouter.Name = "cmdAjouter";
+            this.cmdAjouter.Size = new System.Drawing.Size(106, 23);
+            this.cmdAjouter.TabIndex = 2;
+            this.cmdAjouter.Text = "Ajouter";
+            this.cmdAjouter.UseVisualStyleBackColor = true;
+            this.cmdAjouter.Click += new System.EventHandler(this.CmdAjouter_Click);
+            // 
+            // cmdEffacer
+            // 
+            this.cmdEffacer.Location = new System.Drawing.Point(337, 174);
+            this.cmdEffacer.Name = "cmdEffacer";
+            this.cmdEffacer.Size = new System.Drawing.Size(106, 23);
+            this.cmdEffacer.TabIndex = 3;
+            this.cmdEffacer.Text = "Effacer";
+            this.cmdEffacer.UseVisualStyleBackColor = true;
+            this.cmdEffacer.Click += new System.EventHandler(this.CmdEffacer_Click);
+            // 
+            // cmdSupprimer
+            // 
+            this.cmdSupprimer.Location = new System.Drawing.Point(337, 274);
+            this.cmdSupprimer.Name = "cmdSupprimer";
+            this.cmdSupprimer.Size = new System.Drawing.Size(106, 23);
+            this.cmdSupprimer.TabIndex = 4;
+            this.cmdSupprimer.Text = "Supprimer";
+            this.cmdSupprimer.UseVisualStyleBackColor = true;
+            this.cmdSupprimer.Click += new System.EventHandler(this.CmdSupprimer_Click);
+            // 
+            // cmdQuitter
+            // 
+            this.cmdQuitter.Location = new System.Drawing.Point(337, 369);
+            this.cmdQuitter.Name = "cmdQuitter";
+            this.cmdQuitter.Size = new System.Drawing.Size(106, 23);
+            this.cmdQuitter.TabIndex = 5;
+            this.cmdQuitter.Text = "Quitter";
+            this.cmdQuitter.UseVisualStyleBackColor = true;
+            this.cmdQuitter.Click += new System.EventHandler(this.CmdQuitter_Click);
+            // 
+            // lst1
+            // 
+            this.lst1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lst1.FormattingEnabled = true;
+            this.lst1.ItemHeight = 16;
+            this.lst1.Location = new System.Drawing.Point(42, 144);
+            this.lst1.Name = "lst1";
+            this.lst1.Size = new System.Drawing.Size(177, 180);
+            this.lst1.TabIndex = 6;
+            // 
+            // lst2
+            // 
+            this.lst2.ForeColor = System.Drawing.Color.Blue;
+            this.lst2.FormattingEnabled = true;
+            this.lst2.ItemHeight = 16;
+            this.lst2.Location = new System.Drawing.Point(573, 56);
+            this.lst2.Name = "lst2";
+            this.lst2.Size = new System.Drawing.Size(176, 180);
+            this.lst2.TabIndex = 7;
+            this.lst2.SelectedIndexChanged += new System.EventHandler(this.lst2_SelectedIndexChanged);
+            // 
+            // lblNbEcoles
+            // 
+            this.lblNbEcoles.AutoSize = true;
+            this.lblNbEcoles.Location = new System.Drawing.Point(593, 307);
+            this.lblNbEcoles.Name = "lblNbEcoles";
+            this.lblNbEcoles.Size = new System.Drawing.Size(140, 17);
+            this.lblNbEcoles.TabIndex = 8;
+            this.lblNbEcoles.Text = "écoles sélectionnées";
+            // 
+            // txtNbEcole
+            // 
+            this.txtNbEcole.Location = new System.Drawing.Point(573, 348);
+            this.txtNbEcole.Name = "txtNbEcole";
+            this.txtNbEcole.Size = new System.Drawing.Size(176, 22);
+            this.txtNbEcole.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNbEcole);
+            this.Controls.Add(this.lblNbEcoles);
+            this.Controls.Add(this.lst2);
+            this.Controls.Add(this.lst1);
+            this.Controls.Add(this.cmdQuitter);
+            this.Controls.Add(this.cmdSupprimer);
+            this.Controls.Add(this.cmdEffacer);
+            this.Controls.Add(this.cmdAjouter);
+            this.Controls.Add(this.cbo2);
+            this.Controls.Add(this.cbo1);
+            this.Name = "Form1";
+            this.Text = "ListBox et ComboBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbo1;
+        private System.Windows.Forms.ComboBox cbo2;
+        private System.Windows.Forms.Button cmdAjouter;
+        private System.Windows.Forms.Button cmdEffacer;
+        private System.Windows.Forms.Button cmdSupprimer;
+        private System.Windows.Forms.Button cmdQuitter;
+        private System.Windows.Forms.ListBox lst1;
+        private System.Windows.Forms.ListBox lst2;
+        private System.Windows.Forms.Label lblNbEcoles;
+        private System.Windows.Forms.TextBox txtNbEcole;
+    }
+}
+
---
+++ listbox et combobox/Form1.cs
@@ -0,0 +1,138 @@
+﻿/*
+ * Project: Listbox et Combobox
+ * Date: 20.08.2019
+ * Name: Meili Dmitri
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace listbox_et_combobox
+{
+    public partial class Form1 : Form
+    {
+        int nbecole = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            cbo1.Items.Add("Swisscom");
+            cbo1.Items.Add("Salt");
+            cbo1.Items.Add("Sunrise");
+
+            lst1.Items.Add("CEPM");
+            lst1.Items.Add("CPNV");
+            lst1.Items.Add("ECL");
+            lst1.Items.Add("EPCL");
+            lst1.Items.Add("EPSIC");
+            lst1.Items.Add("ETML");
+
+            cbo2.Items.Add("Gymnase de Bugnon");
+            cbo2.Items.Add("Gymnase de Beaulieu");
+            cbo2.Items.Add("Gymnase d'Yverdon");
+
+            
+
+        }
+
+        private void CmdAjouter_Click(object sender, EventArgs e)
+        {
+
+            
+            if (cbo1.SelectedIndex == -1 && cbo2.SelectedIndex == -1 && lst1.SelectedIndex==-1)/*si aucun element n est selectionner*/
+            {
+                MessageBox.Show("veuillez selectionner une école","error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            if (cbo1.SelectedIndex >= 0)/*si un element est selectionner dans cbo1*/
+            {
+                
+                if (lst2.Items.Contains(cbo1.SelectedItem))/*si l élement sélectionner est deja dans lst2*/
+                {
+                    MessageBox.Show("","",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                }
+                else/*ajout des élement sélectionner*/
+                {
+                    lst2.Items.Add(cbo1.SelectedItem);
+                    cbo1.SelectedIndex = -1;
+                    nbecole++;
+                    txtNbEcole.Text =nbecole.ToString() ;
+                }
+            }
+            if (cbo2.SelectedIndex >= 0)/*si un element est selectionner dans cbo2*/
+            {
+                if (lst2.Items.Contains(cbo2.SelectedItem))/*si l élement sélectionner est deja dans lst2*/
+                {
+                    MessageBox.Show("", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else/*ajout des élement sélectionner*/
+                {
+                    lst2.Items.Add(cbo2.SelectedItem);
+                    cbo2.SelectedIndex = -1;
+                    nbecole++;
+                    txtNbEcole.Text = nbecole.ToString();
+                }
+            }
+            if(lst1.SelectedIndex>= 0)/*si un element est selectionner dans lst1*/
+            {
+                if (lst2.Items.Contains(lst1.SelectedItem))
+                {
+                    MessageBox.Show("", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else/*ajout des élement sélectionner*/
+                {
+                    lst2.Items.Add(lst1.SelectedItem);
+                    lst1.SelectedIndex = -1;
+                    nbecole++;
+                    txtNbEcole.Text = nbecole.ToString();
+                }
+            }
+            
+        }
+
+        private void CmdEffacer_Click(object sender, EventArgs e)
+        {
+            
+            lst2.Items.RemoveAt(lst2.SelectedIndex);//pour effacer
+            nbecole--;
+            txtNbEcole.Text = nbecole.ToString();
+        }
+
+        private void CmdSupprimer_Click(object sender, EventArgs e)
+        {
+            lst2.Items.Clear();//pour supprimer
+            nbecole = 0;
+            txtNbEcole.Text = nbecole.ToString();
+        }
+
+        private void CmdQuitter_Click(object sender, EventArgs e)//pour quitter
+        {
+            DialogResult dialogResult=MessageBox.Show("voulez vous quitter le programme?", "", MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+            }
+        }
+
+        private void lst2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
